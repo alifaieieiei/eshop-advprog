4e04cbc5a68bde05ce26deaacff82b11ad309e6a
alifaieieiei <alifaieieiei@gmail.com>
2026-02-24 18:41:43 +0700

    Test pass

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,7 @@ dependencies {
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
 }
 
