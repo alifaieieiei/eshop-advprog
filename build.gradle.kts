7c9fe63174bf53e553464b2cfa48ba58ca0e6887
alifaieieiei <alifaieieiei@gmail.com>
2026-02-24 18:27:39 +0700

    Add ProductRepositoryTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
 }
 
 tasks.register<Test>("unitTest") {
